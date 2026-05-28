a936db13a76b7af3395c1d45c8984582aab69f14
Caleb Sandfort <caleb@leverageworkshop.com>
2026-05-28 15:11:08 -0700

    fix(LEV-15): force-dynamic on settings page to prevent build-time DB query

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,6 +1,8 @@
 import { getWorkflowList } from '@/lib/workflows/queries'
 import { SettingsClient } from './SettingsClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function SettingsPage() {
   const navWorkflows = await getWorkflowList()
   return <SettingsClient navWorkflows={navWorkflows} />
